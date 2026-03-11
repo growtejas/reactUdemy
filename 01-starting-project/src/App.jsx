fd13960a9ff02ab85108f7418ef27f9f935bbdfd
growtejas <tejaspatiloppo@gmail.com>
2026-03-11 16:10:25 +0530

    state management task

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -7,11 +7,44 @@ import { EXAMPLES } from './data.js';
 
 function App() {
   const [selectedTopic, setSelectedTopic] = useState('components');
+  const [num, setNum] = useState(0);
+
+  const [greet, setGreet] = useState("Welcome, Guest");
+
+  const [isVisible, setIsVisible] = useState(true);
+
+  // const [userName, setUserName] = useState(null);
+
+  // function handleSelect(name){
+  //   setUserName(name)
+  // }
+
+  function handleVisiblity(){
+    setIsVisible((prev) => !prev);
+  }
+  function handleGreet(){
+    // let newGreet = [...greet]
+    // newGreet = "Welcome, User";
+    // setGreet(newGreet)
+    setGreet("Welcome, User")
+  }
 
   function handleSelect(selectedButton){
     setSelectedTopic(selectedButton)
     //console.log(selectedButton);
   }  
+
+  function increase (){
+    setNum(num+1)
+  }
+
+  function decrease (){
+    setNum(num-1)
+  }
+  
+  function increaseByFive (){
+    setNum(num+5)
+  }
   return (
     <div>
       <Header/>
@@ -32,10 +65,10 @@ function App() {
         <section id='examples'>
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect ={()=> handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect ={()=> handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect ={()=> handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect ={()=> handleSelect('state')}>State</TabButton>
+            <TabButton isSelected={selectedTopic === 'components'} onSelect ={()=> handleSelect('components')}>Components</TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} onSelect ={()=> handleSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onSelect ={()=> handleSelect('props')}>Props</TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onSelect ={()=> handleSelect('state')}>State</TabButton>
           </menu>
           <div id='tab-content'>
             <h3>{EXAMPLES[selectedTopic].title}</h3>
@@ -47,6 +80,29 @@ function App() {
             </pre>
           </div>
         </section>
+        <section>
+          <h2>{num}</h2>
+          <h2>{num}</h2>
+          <button onClick={increase}>Increase</button>
+          <button onClick={decrease}>Decrease</button>
+          <button onClick={increaseByFive}>Increase by 5</button>
+        </section>
+        <section>
+          <h1>{greet}</h1>
+          <button onClick={handleGreet}>Change</button>
+        </section>
+        <section>
+          <button onClick = { () => setIsVisible(!isVisible)}>Toggle Secret</button>
+          {isVisible && <p>The secret code is 1234!</p>}
+        </section>
+        {/* <section>
+         
+          <button onClick={() => handleSelect('Max')}>Pass MAX</button>
+          <button onClick={() => handleSelect('Anna')}>Pass ANNA</button>
+          <button onClick={() => handleSelect('Tejas')}>Pass TEJAS</button> 
+
+          <h3>Selected: {userName}</h3>
+        </section> */}
       </main>
     </div>
   );
